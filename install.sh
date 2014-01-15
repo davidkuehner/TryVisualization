e6903b40104915e765b4f8c1acde1aadb1278af2
davidkuhner <david.kuhner@he-arc.ch>
2014-01-15 21:28:56 +0100

    Add install bash script

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo -e "\nThis script will set the environment for tryVisualization \n"
+echo -e "\nWill be installed : \n"
+echo -e "\tphp5-gd library"
+echo -e "\tfirefox library"
+echo -e "\nWill be downloaded : \n"
+echo -e "\tcomposer.phar"
+echo -e "\nWill be runed : \n"
+echo -e "\tcomposer install\n"
+
+echo "Press enter to proceed :"
+read -r nop
+
+echo "Installation of php GD"
+sudo apt-get install php5-gd
+echo -e "\nInstallation of Firefox"
+sudo apt-get install firefox
+echo -e "\nDownload of Composer"
+curl -sS https://getcomposer.org/installer | php
+echo -e "\nExecution of Composer install"
+php composer.phar install
+
+echo -e "\nEverything is installed."
+echo -e "Try visualization: php tryVisualization.php \"[REGEX]\"\n"
