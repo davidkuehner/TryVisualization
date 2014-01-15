1b1b3e33fea0871b8a38a993bf7883c3799be766
davidkuhner <david.kuhner@he-arc.ch>
2014-01-15 23:17:47 +0100

    Mod change install to be less intrusif

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo -e "\nThis script will set the environment for tryVisualization \n"
-echo -e "\nWill be installed : \n"
+echo -e "\nCheck if installed : \n"
 echo -e "\tphp5-gd library"
 echo -e "\tphp5-json library"
 echo -e "\tfirefox"
@@ -13,12 +13,43 @@ echo -e "\tcomposer install\n"
 echo "Press enter to proceed :"
 read -r nop
 
-echo "Installation of Php GD"
-sudo apt-get install php5-gd
-echo "Installation of Php json"
-sudo apt-get install php5-json
-echo -e "\nInstallation of Firefox"
-sudo apt-get install firefox
+stop_install=false
+
+echo "Check of Php GD"
+result=$(dpkg -s php5-gd | grep Status)
+echo $result
+if [ "$result" != "Status: install ok installed" ] ; 
+	then 
+		echo "Please install php5-gd"
+		echo "eg: sudo apt-get install php5-gd"
+		stop_install=true
+fi
+
+echo -e "\nCheck of Php json"
+result=$(dpkg -s php5-json | grep Status)
+echo $result
+if [ "$result" != "Status: install ok installed" ] ; 
+	then 
+		echo "Please install php5-json"
+		echo "eg: sudo apt-get install php5-json"
+		stop_install=true
+fi
+
+echo -e "\nCheck of Firefox"
+result=$(dpkg -s firefox | grep Status)
+echo $result
+if [ "$result" != "Status: install ok installed" ] ; 
+	then 
+		echo "Please install firefox"
+		echo "eg: sudo apt-get install firefox"
+		echo -e "Or change the tryVisualization script to use your favourit browser\n"
+		stop_install=true
+fi
+
+if $stop_install ; then
+    exit
+fi
+
 echo -e "\nDownload of Composer"
 curl -sS https://getcomposer.org/installer | php
 echo -e "\nExecution of Composer install"
